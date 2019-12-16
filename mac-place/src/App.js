fda7f498004780f8d487587d90ecf15e32ab1fd7
jandird <jandird@mcmaster.ca>
2019-12-16 02:30:48 -0500

    Got BitMap created

--- mac-place/src/App.js
+++ mac-place/src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Bit from './Bit.js'
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.createMap = this.createMap.bind(this);
+    }
+
+    render() {
+        let bits = [];
+        for (let i = 0; i < 40; i++){
+            for (let j = 0; j < 84; j++){
+                let bitStyle = {
+                    background: 'black',
+                    top: i*20 + 'px',
+                    left: j*20 + 'px'
+                };
+                bits.push(<Bit key={i + j} style={bitStyle}/>)
+            }
+        }
+
+        return (
+            <div className="App">
+                {bits}
+            </div>
+        );
+    }
+
+    createMap() {
+        let bits = [];
+        for (let i = 0; i < 1000; i++){
+            for (let j = 0; j < 1000; j++){
+                let bitStyle = {
+                    background: 'black',
+                    top: i*20 + 'px',
+                    left: j*20 + 'px'
+                };
+                bits.push(<Bit style={bitStyle}/>)
+            }
+        }
+        return <tbody>{bits}</tbody>;
+    }
 }
 
 export default App;
---
+++ mac-place/src/Bit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './resources/css/bit.css'
+
+function Bit(props){
+    return (
+        <div className="bit" style={props.style}/>
+    );
+}
+
+export default Bit;
