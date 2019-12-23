272036d1d6807d1c3428a775d981ae14bef0b6fe
jandird <jandird@mcmaster.ca>
2019-12-22 23:08:58 -0500

    Got link to firebase working

--- mac-place/src/App.js
+++ mac-place/src/App.js
@@ -2,26 +2,53 @@ import React from 'react';
 import './App.css';
 
 import Bit from './Bit.js'
+import Fire from './config.js'
 
 class App extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            bits: []
+        };
+
+        this.updateBits = this.updateBits.bind(this);
+    }
+
+    componentDidMount() {
+        this.updateBits();
+        // for (let i = 0; i <= 39; i++){
+        //     for (let j = 0; j <= 83; j++){
+        //         Fire.database().ref("macplace").push({m: i, n: j, color: "#000000"})
+        //     }
+        // }
     }
 
     render() {
-        let bits = [];
-        for (let i = 0; i < 40; i++){
-            for (let j = 0; j < 84; j++){
-                bits.push(<Bit key={i + j} m={i} n={j} color={'black'}/>)
-            }
-        }
-
         return (
             <div className="App">
-                {bits}
+                {this.state.bits}
             </div>
         );
+    }
+
+    updateBits(){
+        let bits = [];
+
+        let rows;
+        let messagesRef = Fire.database().ref('macplace').orderByKey();
+        messagesRef.on('value', snapshot => {
+            rows = snapshot.val();
+            for (let bit in rows){
+                if (rows.hasOwnProperty(bit)){
+                    let obj = rows[bit];
+                    bits.push(<Bit key={bit} m={obj.m} n={obj.n} color={obj.color}/>);
+                }
+            }
+
+            this.setState({bits: bits});
+        });
     }
 }
 
